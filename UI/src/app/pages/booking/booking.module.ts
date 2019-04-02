1b83dea62b0e6479bc235b268249ccdf87aae11a
Lukasz <lukaporebski@gmail.com>
2019-04-02 13:44:01 +0200

    Removed screening rooms

--- UI/src/app/pages/booking/booking.module.ts
+++ UI/src/app/pages/booking/booking.module.ts
@@ -1,15 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {BookingComponent} from './booking.component';
-import {ScreeningRoomTwoComponent} from './components/screening-room-two/screening-room-two.component';
-import {ScreeningRoomOneComponent} from './components/screening-room-one/screening-room-one.component';
 import { ScreeningRoomComponent } from './components/screening-room/screening-room.component';
 
 @NgModule({
   declarations: [
     BookingComponent,
-    ScreeningRoomTwoComponent,
-    ScreeningRoomOneComponent,
     ScreeningRoomComponent
   ],
   imports: [
--- UI/src/app/pages/booking/components/screening-room/services/screening-room.service.ts
+++ UI/src/app/pages/booking/components/screening-room/services/screening-room.service.ts
@@ -16,42 +16,38 @@ export class ScreeningRoomService {
       screeningRoomName: 'Pierwsza',
       rows: [
         {
-          number: 1,
           seats: [
-            {id: '', number: 0, isSeat: false},
-            {id: '12', number: 1, isSeat: true},
-            {id: '13', number: 2, isSeat: true},
-            {id: '', number: 0, isSeat: false},
-            {id: '15', number: 3, isSeat: true}
+            {id: '', isSeat: false},
+            {id: '12', isSeat: true},
+            {id: '13', isSeat: true},
+            {id: '', isSeat: false},
+            {id: '15', isSeat: true}
           ]
         },
         {
-          number: 2,
           seats: [
-            {id: '21', number: 0, isSeat: false},
-            {id: '22', number: 1, isSeat: true},
-            {id: '23', number: 2, isSeat: true},
-            {id: '', number: 0, isSeat: false},
-            {id: '', number: 0, isSeat: false},
+            {id: '21', isSeat: false},
+            {id: '22', isSeat: true},
+            {id: '23', isSeat: true},
+            {id: '', isSeat: false},
+            {id: '', isSeat: false},
           ]
         },
         {
-          number: 3,
           seats: [
-            {id: '', number: 0, isSeat: false},
-            {id: '', number: 0, isSeat: false},
-            {id: '', number: 0, isSeat: false},
-            {id: '', number: 0, isSeat: false},
-            {id: '33', number: 1, isSeat: true}
+            {id: '', isSeat: false},
+            {id: '', isSeat: false},
+            {id: '', isSeat: false},
+            {id: '', isSeat: false},
+            {id: '33', isSeat: true}
           ]
         },
         {
-          number: 4,
           seats: [
-            {id: '41', number: 1, isSeat: true},
-            {id: '42', number: 2, isSeat: true},
-            {id: '', number: 0, isSeat: false},
-            {id: '44', number: 3, isSeat: true}
+            {id: '41', isSeat: true},
+            {id: '42', isSeat: true},
+            {id: '', isSeat: false},
+            {id: '44', isSeat: true}
           ]
         },
       ]
