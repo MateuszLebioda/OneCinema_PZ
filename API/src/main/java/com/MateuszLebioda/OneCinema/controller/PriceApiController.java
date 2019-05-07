9addb68c16f0b9a55677ceed4bee3a2a7e217ede
Mateusz Lebioda <mateusz.lebioda.ml@gmail.com>
2019-05-07 15:14:06 +0200

    Added ApiOperation Swagger in Proce Api

--- API/src/main/java/com/MateuszLebioda/OneCinema/controller/PriceApiController.java
+++ API/src/main/java/com/MateuszLebioda/OneCinema/controller/PriceApiController.java
@@ -3,6 +3,8 @@ package com.MateuszLebioda.OneCinema.controller;
 import com.MateuszLebioda.OneCinema.service.Formatter;
 import com.MateuszLebioda.OneCinema.service.PriceService.PriceService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.wordnik.swagger.annotations.ApiModelProperty;
+import com.wordnik.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RequestMapping
-@RestController
+@RestController("/price")
 public class PriceApiController {
 
     @Autowired
@@ -20,7 +22,8 @@ public class PriceApiController {
     @Autowired
     Formatter formatter;
 
-    @RequestMapping(value = "/price", method = RequestMethod.GET)
+    @ApiOperation(value = "Return price list")
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
     public String spot() throws JsonProcessingException {
         return  formatter.returnJson(priceService.getPriceList());
