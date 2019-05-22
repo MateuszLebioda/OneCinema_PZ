770608ad00f6136cde4bea1db35e0c1cb2572ef6
Mateusz Lebioda <mateusz.lebioda.ml@gmail.com>
2019-05-22 20:38:55 +0200

    Added Api to get all genders

--- API/src/main/java/com/MateuszLebioda/OneCinema/controller/TypeController.java
+++ API/src/main/java/com/MateuszLebioda/OneCinema/controller/TypeController.java
@@ -1,13 +1,32 @@
 package com.MateuszLebioda.OneCinema.controller;
 
 
+import com.MateuszLebioda.OneCinema.entity.Type;
+import com.MateuszLebioda.OneCinema.entity.TypeRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.wordnik.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/genders")
 @RestController
 public class TypeController {
 
-   /* @Autowired
-    TypeRepository typeRepository;*/
+    @Autowired
+    TypeRepository typeRepository;
 
+
+    @ApiOperation(value = "Return all genders")
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    @ResponseBody
+    public List<String> getGenders() throws JsonProcessingException {
+        List<String> genders = new ArrayList<>();
+        for (Type type:typeRepository.findAll()){
+            genders.add(type.getName());
+        }
+        return genders;
+    }
 }
