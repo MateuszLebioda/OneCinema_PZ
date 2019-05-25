7fd862d95f836bba4060c2802621a275585eea03
Mateusz Lebioda <mateusz.lebioda.ml@gmail.com>
2019-05-25 22:44:06 +0200

    Added api to reserved spots by id.

--- API/src/main/java/com/MateuszLebioda/OneCinema/entity/ReservationRepository.java
+++ API/src/main/java/com/MateuszLebioda/OneCinema/entity/ReservationRepository.java
@@ -1,9 +1,13 @@
 package com.MateuszLebioda.OneCinema.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation,String> {
     List<Reservation>findAllBySeance(Seance seance);
+
+    @Query("select r from Reservation r join  r.seance s where s.id = :id")
+    List<Reservation> findAllBySeanceId(String id);
 }
