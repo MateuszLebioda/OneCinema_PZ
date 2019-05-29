9b2f65047f5dcdedc6f803708699827fd81bd211
Mateusz Lebioda <mateusz.lebioda.ml@gmail.com>
2019-05-29 20:52:08 +0200

    Fixes changed charset to utf8

---
+++ API/src/main/java/com/MateuszLebioda/OneCinema/configuration/CodingConfiguration.java
@@ -0,0 +1,23 @@
+package com.MateuszLebioda.OneCinema.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class CodingConfiguration implements WebMvcConfigurer {
+
+    private static final Charset UTF8 = Charset.forName("UTF-8");
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
+        stringConverter.setSupportedMediaTypes(Arrays.asList(new MediaType("text", "plain", UTF8)));
+        converters.add(stringConverter);
+    }
+}
