efa005ebfae0f472065b007c5c55e2884c8ccf98
Mateusz Lebioda <mateusz.lebioda.ml@gmail.com>
2019-06-03 11:28:25 +0200

    Fixed 500 in get /getBySeanceId/{id}

--- API/src/main/java/com/MateuszLebioda/OneCinema/controller/ReservationController.java
+++ API/src/main/java/com/MateuszLebioda/OneCinema/controller/ReservationController.java
@@ -2,11 +2,14 @@ package com.MateuszLebioda.OneCinema.controller;
 
 
 import com.MateuszLebioda.OneCinema.Model.Book.BookingRequestModel;
+import com.MateuszLebioda.OneCinema.exception.CannotFindReservationException;
 import com.MateuszLebioda.OneCinema.service.EmailService;
 import com.MateuszLebioda.OneCinema.service.ReservationService;
 import com.MateuszLebioda.OneCinema.utils.formatters.Formatter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,8 +31,12 @@ public class ReservationController {
     //@ApiOperation(value = "Return reserved spots")
     @RequestMapping(value = "/getBySeanceId/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public List<String> getReserved(@PathVariable String id){
-        return  reservationService.getReservationById(id);
+    public ResponseEntity getReserved(@PathVariable String id) throws JsonProcessingException {
+        try {
+            return new ResponseEntity(formatter.returnJson(reservationService.getReservationById(id)), HttpStatus.OK);
+        } catch (CannotFindReservationException e) {
+            return new ResponseEntity("Cannot find reservation",HttpStatus.BAD_REQUEST);
+        }
     }
 
     //@ApiOperation(value = "Book spots")
